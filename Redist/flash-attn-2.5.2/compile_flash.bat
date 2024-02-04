59ce39141638e604ed0a3aa751c73552fedac38e
Shaltiel Shmidman <shaltieltzion@gmail.com>
2024-02-04 21:00:50 +0200

    Added compiled runtimes

--- Redist/flash-attn-2.5.2/compile_flash.bat
+++ Redist/flash-attn-2.5.2/compile_flash.bat
@@ -6,4 +6,4 @@ mkdir build
 cd build
 cmake .. -DLIBTORCH_PATH=..\..\libtorch-cuda-12.1\libtorch-cuda-12.1\libtorch-win-shared-with-deps\libtorch
 cmake --build . --config Release --target ALL_BUILD
-copy Release\* ..\..\compiled-runtimes\win-x64\native\
+copy Release\* ..\..\..\compiled-runtimes\win-x64\native\
--- Redist/flash-attn-2.5.2/compile_flash.sh
+++ Redist/flash-attn-2.5.2/compile_flash.sh
@@ -6,4 +6,4 @@ mkdir build
 cd build
 cmake .. -DLIBTORCH_PATH=../../libtorch-cuda-12.1/libtorch-cuda-12.1/libtorch-shared-with-deps/libtorch -DCUDA_TOOLKIT_ROOT_DIR=$CUDA_TOOLKIT_ROOT_DIR
 cmake --build . --config Release
-cp libflash_attn.so ../../compiled-runtimes/linux-x64/native/libflash_attn.so
+cp libflash_attn.so ../../../compiled-runtimes/linux-x64/native/libflash_attn.so
